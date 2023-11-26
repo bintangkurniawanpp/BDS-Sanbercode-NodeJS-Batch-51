0c2fb8c0c6bd0a01f5290add8ca967cd11741826
bintang <nnew34004@gmail.com>
2023-11-26 10:55:16 +0700

    release 1 done

--- Quiz-3/app/Validators/GenreValidator.ts
+++ Quiz-3/app/Validators/GenreValidator.ts
@@ -24,7 +24,7 @@ export default class GenreValidator {
    *    ```
    */
   public schema = schema.create({
-    nama: schema.string({ trim: true }, [rules.required()])
+    name: schema.string({ trim: true }, [rules.required()])
   })
 
   /**
