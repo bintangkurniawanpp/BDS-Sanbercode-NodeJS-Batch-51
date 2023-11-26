3e366f14f44f82f8ba6114ed823210f82a1ac429
bintang <nnew34004@gmail.com>
2023-11-26 09:47:10 +0700

    release 0 done

--- quiz-3/database/migrations/1700966692403_genres.ts
+++ quiz-3/database/migrations/1700966692403_genres.ts
@@ -5,9 +5,9 @@ export default class extends BaseSchema {
 
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id');
-      table.string('name').notNullable();
-      table.timestamps(true, true);
+      table.increments('id')
+      table.string('name').notNullable()
+      table.timestamps(true, true)
 
     })
   }
